7207318ade072955a9d01b950b53e76b533dd3a6
Your Name <luis.rivass@utem.cl>
2022-07-15 01:39:30 -0400

    First Commit "15-07-2022"

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+class LoginPage extends StatefulWidget {
+ LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State LoginPage> createState() =>  LoginPageState();
+}
+
+class  LoginPageState extends State LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
